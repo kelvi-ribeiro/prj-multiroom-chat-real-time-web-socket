6c0097cdb496f6126c03968dcdaf8e825be1fcc9
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2018-12-27 19:29:41 -0200

    Aula 62. Recebendo e validando os dados do formulario de entrada

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,12 @@
 module.exports.iniciaChat = function(app,req,res){
-    res.render('chat')
+    var dadosForm = req.body;
+    req.assert('apelido','Nome ou Apelido é obrigatório').notEmpty();
+    req.assert('apelido','Nome ou Apelido deve conter entre 3 e 15 caracteres').len(3,15);
+
+    var erros = req.validationErrors();
+    if(erros){
+        res.render('index',{validacao:erros});
+    }
+
+    res.render('chat');
 }
