34f63aacb5269cc65ccc5580498bb0fdfa5a40f8
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2018-12-28 04:51:32 -0200

    Aula 66. Abrindo a conexao via websocket

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ var server = app.listen(80,function(){
     console.log('Servidor online');
 });
 
-require('socket.io').listen(server);
+var io = require('socket.io').listen(server);
+
+/* Criar a conexão por websocket */
+io.on('connection',function(socket){
+    console.log('Usuário Conectou');
+    socket.on('disconnect',function(){
+        console.log('Usuário desconectou');
+    });
+});
